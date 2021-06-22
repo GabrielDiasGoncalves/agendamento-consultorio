adfc4745378a50487eea1fa667b09acfd97abd62
Gabriel Dias Gonçalves <gabrieldgon@gmail.com>
2021-06-22 20:41:44 -0300

    Adicionando primeiras classes modelo

---
+++ src/AgendamentoConsultorio/AgendamentoConsultorio.Domain/Models/Patient.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AgendamentoConsultorio.Domain.Models
+{
+    public class Patient
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public char Gender { get; set; }
+        public string CPF { get; set; }
+        public string Address { get; set; }
+        public string CEP { get; set; }
+    }
+}
---
+++ src/AgendamentoConsultorio/AgendamentoConsultorio.Domain/Models/Scheduling.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AgendamentoConsultorio.Domain.Models
+{
+    public class Scheduling
+    {
+        public DateTime DayOfConsult { get; set; }
+        public int IDDoctor { get; set; }
+        public Doctor Doctor { get; set; }
+        public int IdPatient { get; set; }
+        public Patient Patient { get; set; }
+    }
+}
